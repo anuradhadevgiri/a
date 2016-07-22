0e53a9261fdf5582d847893908896e9f11a27192
anuradhadevgiri <anuradhasd93@gmail.com>
2016-07-22 08:54:59 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,22 @@
+public class Primitive{
+	public static void main(String[] args){
+		byte b=12;
+		short s=345;
+		int i=2343;
+        long l=12367.9;
+         float= f=2346.90;
+          double d=345.9;
+          boolean	bl= Ttue;
+		  char c='m';
+		  System.out.println("byte="b);
+		  System.out.println("short="s);
+		  System.out.println("int="i);
+		  System.out.println("long="l);
+		  System.out.println("float="f);
+		  System.out.println("double="d);
+		  System.out.println("boolean="bl);
+		  System.out.println("char="c);
+		  
+		
+	}
+}
